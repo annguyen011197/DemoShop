91b8410423a36fc051e435ae5ee1dfbb89606c9f
Nguyen Tran Minh An <tranminhannguyen@gmail.com>
2017-09-26 22:08:40 +0700

    header v2

--- src/cpn/Main/Shop/Header.js
+++ src/cpn/Main/Shop/Header.js
@@ -13,16 +13,20 @@ const { height } = Dimensions.get('window');
 
 export default class Header extends Component {
     render() {
-        const { iconContainer,
+        const { iconContainerLeft,
+            iconContainerRight,
             container,
+            iconContainer,
             textContainer,
             icon,
             text,
-            textInput
+            textInput,
+            textInputContainer
                } = styles;
         return (
-            <View style={{ height: height / 8, backgroundColor: 'cyan' }}>
+            <View style={{ height: height / 7, backgroundColor: '#568fea', alignItems: 'center' }}>
                 <View style={container}>
+                    <View style={iconContainerLeft}>
                     <TouchableOpacity
                     onPress={this.props.openMenu}
                     style={iconContainer}
@@ -30,22 +34,28 @@ export default class Header extends Component {
                         <Image
                         source={icMenu}
                         style={icon}
-    
                         />
                     </TouchableOpacity>
+                    </View>
                     <View style={textContainer}>
                         <Text style={text}>Wearing a Dress</Text>
                     </View>
+                    <View style={iconContainerRight}>
                     <View style={iconContainer}>
-                        <Image
+                    <Image
                         source={icLogo}
                         style={icon}
-                        />
+                    />
+                    </View>
                     </View>
                 </View>
+                <View style={textInputContainer}>
                 <TextInput
                  style={textInput}
+                 underlineColorAndroid='transparent'
+                 placeholder='What do you want to buy ?'
                 />
+                </View>
             </View>            
         );
     }
@@ -56,24 +66,31 @@ const styles = StyleSheet.create({
         backgroundColor: '#568fea',
         flexDirection: 'row',
         justifyContent: 'space-between',
-        flex: 7
+        flex: 5,
+        width: '90%'
+    },
+    iconContainerLeft: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-start'
+    },
+    iconContainerRight: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-end'
     },
     iconContainer: {
-        // width: height / 20,
-        // height: height / 20,
-       // margin: 10,
-        flex: 1,
-        justifyContent: 'space-around',
+        width: '50%', 
+        alignSelf: 'center'
     },
     textContainer: {
         flex: 3,
         justifyContent: 'space-around'
     },
     icon: {
-        alignSelf: 'center',
-        alignContent: 'stretch',
-        height: '80%',
-        width: '40%',
+        height: '70%', 
+        width: '80%',
+        alignItems: 'center'
     },
     text: {
         margin: 10,
@@ -82,6 +99,16 @@ const styles = StyleSheet.create({
         color: 'white',
     },
     textInput: {
-        flex: 3
+        backgroundColor: 'white',
+        width: '100%',
+        height: '80%',
+        textAlignVertical: 'center',
+        paddingLeft: 10                        
     },
+    textInputContainer: {
+        flex: 5,
+        backgroundColor: '#568fea',
+        alignItems: 'center',
+        width: '90%'
+    }
 });
